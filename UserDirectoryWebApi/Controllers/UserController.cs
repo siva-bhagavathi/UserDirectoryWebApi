2fd7943b6e019bd12d618e9c9263cff0650b476a
Siva <siva.bhagavathiperumal@gmail.com>
2025-03-04 16:32:45 +1100

    Add project files.

---
+++ UserDirectoryWebApi/Controllers/UserController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using UserDirectoryWebApi.Models;
+
+namespace UserDirectoryWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly string _basePath= "C:\\Users\\sivan\\source\\Projects\\UserDirectory_Ceva\\Users";
+        //Alternatively the above path can be read from appsettings.json and configured
+        public UserController()
+        {
+            
+        }
+
+        [HttpPost]
+        public IActionResult CreateDirectory(UserRequest[] userRequests)
+        {
+            if(userRequests == null || userRequests.Length==0)
+            {
+                return BadRequest("Users cannot be null");
+            }
+
+            string usersPath = Path.Combine(_basePath, "Users");
+            if (!Directory.Exists(_basePath))
+            {
+                Directory.CreateDirectory(_basePath);
+            }
+
+            foreach (var request in userRequests)
+            {
+                if (string.IsNullOrEmpty(request.EmployeeID))
+                {
+                    return BadRequest("EmployeeID is required.");
+                }// similarly other validations can be added or a separate validation filter can be created
+
+                string userInPath = Path.Combine(usersPath, request.EmployeeID, "IN");
+                if (!Directory.Exists(userInPath))
+                {
+                    Directory.CreateDirectory(userInPath);
+                }
+
+                // To Store the request as a JSON file in the "IN" directory
+                string jsonFilePath = Path.Combine(userInPath, "request.json");
+                System.IO.File.WriteAllText(jsonFilePath, JsonConvert.SerializeObject(request));
+            }
+
+
+            return Ok("Directory created and user added successfully");
+        }
+    }
+}
---
+++ UserDirectoryWebApi/Models/UserRequest.cs
@@ -0,0 +1,20 @@
+﻿namespace UserDirectoryWebApi.Models
+{
+    public class UserRequest
+    {
+        //public required string[] Users { get; set; }
+        public int ID { get; set; }
+        public int UserID { get; set; }
+        public string? EmployeeID { get; set; }
+        public string? SiteName { get; set; }
+        public string? BusinessUnitName { get; set; }
+        public string? AccountName { get; set; }
+        public string? GroupName { get; set; }
+        public string? CategoryName { get; set; }
+        public string? TypeName { get; set; }
+        public DateTime Date { get; set; }
+        public string? Duration { get; set; }
+        public bool IsProcessed { get; set; }        
+
+    }
+}
